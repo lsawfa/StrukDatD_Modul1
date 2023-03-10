7bf0df6dc1ad9a3bb029e3538d9a1ae206822c5e
Larynt Sawfa Kenanga <109521914+lsawfa@users.noreply.github.com>
2023-03-10 08:05:55 +0700

    Push pertama

---
+++ Modul1/src/Kasus2/Hidangan.java
@@ -0,0 +1,20 @@
+/**
+ * @author Larynt Sawfa Kenanga
+ */
+package Kasus2;
+
+public class Hidangan {
+    protected String namaHidangan;
+
+    public String getNamaHidangan() {
+        return namaHidangan;
+    }
+
+    public void setNamaHidangan(String namaHidangan) {
+        this.namaHidangan = namaHidangan;
+    }
+
+    public String disantap(){
+        return "Makanan Dihidangkan";
+    }
+}
---
+++ Modul1/src/Kasus2/Makanan.java
@@ -0,0 +1,10 @@
+/**
+ * @author Larynt Sawfa Kenanga
+ */
+package Kasus2;
+
+public class Makanan extends Hidangan{
+    public String disantap(){
+        return this.getNamaHidangan()+" dimakan";
+    }
+}
